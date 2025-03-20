65d100c38c65e18e65a95f4b566332d40fbe25f9
Yanie <maanadelacruz@gbox.ncf.edu.ph>
2025-03-20 23:15:32 +0800

    Authentication Updates

--- app/application/auth/auth.py
+++ app/application/auth/auth.py
@@ -12,17 +12,20 @@ def is_valid_email(email):
 def register():
     data = request.json
     
-    if not data or not all(key in data for key in ["name", "email", "password", "encryption_key"]):
+    if not data or not all(key in data for key in ["name", "email", "accessCode", "password", "confirmPassword"]):
         return jsonify({"error": "Missing required fields"}), 400
-
-    if data["encryption_key"] != cipher.REGISTRATION_KEY:
-        return jsonify({"error": "Invalid encryption key"}), 403
 
     if not is_valid_email(data["email"]):
         return jsonify({"error": "Invalid email format"}), 400
 
+    if data["accessCode"] != cipher.REGISTRATION_KEY:
+        return jsonify({"error": "Invalid encryption key"}), 403
+
     if len(data["password"]) < 6:
         return jsonify({"error": "Password must be at least 6 characters long"}), 400
+    
+    if data["password"] != data["confirmPassword"]:
+        return jsonify({"error": "Passwords do not match"}), 400    
     
     if database.users_collection.find_one({"email": data["email"]}):
         return jsonify({"error": "User already exists"}), 400
@@ -34,7 +37,6 @@ def register():
 
 def login():
     data = request.json
-    print(data)
     print(f"[DEBUG] Received data: {data}")
     
     if not data or not all(key in data for key in ["email", "password"]):
